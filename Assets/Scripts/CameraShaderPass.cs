b6b505eb1d35f2214ff94f33bdc38cf286e2a7ba
Clement DUSSOL <clement.dussol@gmail.com>
2019-10-08 09:03:08 +0200

    first commit

---
+++ Assets/Scripts/CameraShaderPass.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShaderPass : MonoBehaviour
+{
+	public Material[] materials;
+	private void OnRenderImage(RenderTexture src, RenderTexture dest)
+	{
+		foreach (Material mat in materials)
+		{
+			Graphics.Blit(src, dest, mat);
+		}
+	}
+}
